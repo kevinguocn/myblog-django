1101263d4c14f0068ea1d7276447229f71f6eb22
kevinguo <kevinguocn@foxmail.com>
2019-03-20 23:39:54 +0800

    用户组的添加删除修改

--- admin/urls.py
+++ admin/urls.py
@@ -6,6 +6,8 @@ urlpatterns=[
     #组管理
     path('basic/group/',views_group.GroupIndexView.as_view(),name='group_index'),
     path('basic/group/create/',views_group.GroupCreateView.as_view(), name='group_create'),
+    path('basic/group/list/',views_group.GroupListView.as_view(), name='group_list'),
+    path('basic/group/delete/',views_group.GroupDeleteView.as_view(), name='group_delete'),
     #Admin 首页
     path('', views.AdminIndexView.as_view(), name='index')
 ]
--- admin/views_group.py
+++ admin/views_group.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.views.generic.base import View, TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from mixin import AdminLoginRequiredMixin
 from user.models import Group
@@ -11,15 +11,39 @@ class GroupIndexView(AdminLoginRequiredMixin,TemplateView):
 
 class GroupCreateView(AdminLoginRequiredMixin,View):
     def get(self,request):
-        ret = dict(groups = Group.objects.all())
+        ret={}
+        if 'id' in request.GET and request.GET['id']:
+            id = request.GET['id']
+            group = get_object_or_404(Group,pk=int(id))
+            ret = dict(group = group)
         return render(request,'admin/group/group_create.html',ret)
 
     def post(self,request):
         res = dict(result=False)
-        group = Group()
+        if 'id' in request.POST and request.POST['id']:
+            group = get_object_or_404(Group,pk=int(request.POST['id']))
+        else:
+            group = Group()
         group_form = GroupForm(request.POST,instance=group)
         if group_form.is_valid():
             group_form.save()
             res['result']=True
-
+        print(res)
+        print(group_form.errors)
         return JsonResponse(res)
+
+
+class GroupListView(AdminLoginRequiredMixin,View):
+    def get(self,request):
+        fields = ['id','name','desc']
+        ret = dict(data = list(Group.objects.values(*fields)))
+        return JsonResponse(ret)
+
+class GroupDeleteView(AdminLoginRequiredMixin,View):
+    def post(self,request):
+        ret=dict(result=False)
+        if "id" in request.POST and request.POST['id']:
+            id_list = map(int,request.POST['id'].split(','))
+            Group.objects.filter(id__in=id_list).delete()
+            ret['result']=True
+        return JsonResponse(ret)
